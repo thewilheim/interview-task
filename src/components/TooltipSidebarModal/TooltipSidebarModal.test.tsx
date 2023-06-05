bd8bb049c603a531955e3eca3d0642f0121a8468
thewilheim <willabraham1@gmail.com>
2023-06-05 16:49:11 +1000

    added boilerplate test cases

--- src/components/TooltipSidebarModal/TooltipSidebarModal.test.tsx
+++ src/components/TooltipSidebarModal/TooltipSidebarModal.test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import TooltipSidebarModal from '.';
+import userEvent from '@testing-library/user-event';
 
-test('renders TooltipSidebarModal', () => {
-  render(<TooltipSidebarModal />);
-  const linkElement = screen.getByText('TooltipSidebarModal');
-  expect(linkElement).toBeInTheDocument();
-});
+const textBody = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro beatae reprehenderit labore.";
+
+describe('<TooltipSidebarModal />', () => {
+  test('renders TooltipSidebarModal as hyperlink', () => {
+    render(<TooltipSidebarModal informationText={textBody} hyperLinkText="Click here for more information" isHyperLink={true} informationTitle="Test Title"/>);
+      const hyperLinkElement = screen.getByText('Click here for more information');
+      expect(hyperLinkElement).toBeInTheDocument();
+  });
+  test('renders TooltipSidebarModal usind information icon', () => {
+    render(<TooltipSidebarModal informationText={textBody} hyperLinkText="Click here" informationTitle="Test Title"/>);
+      const infomationIconElement = screen.getAllByTestId('infoIcon');
+      expect(infomationIconElement).toBeInTheDocument();
+  });
+
+  test('renders TooltipSidebarModal using  unfilled information icon', () => {
+    render(<TooltipSidebarModal informationText={textBody} hyperLinkText="Click here" informationTitle="Test Title" isUnFilledIcon={true}/>);
+      const infomationIconElement = screen.getAllByTestId('infoIconUnFilled');
+      expect(infomationIconElement).toBeInTheDocument();
+  });
+  test('opens modal to display information', () => {
+    render(<TooltipSidebarModal informationText={textBody} hyperLinkText="Click here for more information" isHyperLink={true} informationTitle="Test Title"/>);
+      const hyperLinkElement = screen.getByText('Click here for more information');
+      expect(hyperLinkElement).toBeInTheDocument();
+      userEvent.click(hyperLinkElement)
+      expect(screen.getByText(textBody)).toBeInTheDocument();
+  });
+})
